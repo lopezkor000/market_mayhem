79226c9129ec7594d601f1afecc329e782640959
Kory <74568018+lopezkor000@users.noreply.github.com>
2025-07-18 01:12:14 -0500

    game start options

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col w-1/4 self-center gap-12 mt-5">
+      <a
+        href="/host"
+        className="text-3xl bg-blue-500 rounded font-bold hover:text-white p-3 cursor-pointer"
+      >
+        Host Game
+      </a>
+      <a
+        href="/join"
+        className="text-3xl bg-blue-500 rounded font-bold hover:text-white p-3 cursor-pointer"
+      >
+        Join Game
+      </a>
+    </div>
+  );
 }
